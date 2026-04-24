0bc9d3973a6c33b0e984edb93c55b8a6a5ef38d3
wampixel <wampixel@gmail.com>
2026-04-24 23:49:35 +0200

    [FIX] moved telemetry configuration here for all projects

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Khronika
+
 We can see all things komrad
+
+## Usage
+
+Add to your project this line
+`khronika = { git = "https://github.com/komrad-company/Khronika.git" }`
+
+On your code, you must add the configuration and call on your main the function `initialize_logger`
+
+now you can call `khronika::error!()` for example.
---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+pub use tracing::{debug, error, info, warn, trace};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+
+pub mod configuration;
+pub mod telemetry;
+
+use crate::configuration::{TelemetryConfiguration, TelemetryOutput};
+
+pub fn intialize_logger(config: TelemetryConfiguration) {
+    let filter = EnvFilter::new(config.level.as_str());
+    let registry = tracing_subscriber::registry().with(filter);
+
+  let (file, _) = match config.output {
+        TelemetryOutput::File { file } => (Some(file), None),
+        TelemetryOutput::Remote { telemetry } => (None, Some(telemetry)),
+        TelemetryOutput::Both { file, telemetry } => (Some(file), Some(telemetry)),
+    };
+
+    let stderr_layer = tracing_subscriber::fmt::layer()
+        .json()
+        .with_writer(std::io::stderr);
+
+    let file_layer = file.map(|path| {
+        let appender =
+            tracing_appender::rolling::never(path.parent().unwrap(), path.file_name().unwrap());
+
+        tracing_subscriber::fmt::layer()
+            .json()
+            .with_writer(appender)
+    });
+
+    // let remote = telemetry_url.map(|url| {});
+
+    registry.with(stderr_layer).with(file_layer).init();
+}
